6e17b8dc2dafb0079647c86379a114358e6b8ca0
RenTensei <scs.bon7@gmail.com>
2023-03-15 15:55:31 +0200

    added solution task 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const renderedGallery = galleryItems
+  .map(
+    ({ original, preview, description }) => `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `
+  )
+  .join('');
+
+galleryRef.innerHTML = renderedGallery;
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
