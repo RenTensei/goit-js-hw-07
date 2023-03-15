2108dd02885f1cd3bc9ca7c246c026feaabc0d4a
RenTensei <scs.bon7@gmail.com>
2023-03-15 13:55:48 +0200

    completed task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const renderedGallery = galleryItems
+  .map(
+    (item) => `
+      <div class="gallery__item">
+        <a 
+          class="gallery__link"
+          href="${item?.original}"
+        >
+            <img
+              class="gallery__image"
+              src="${item?.preview}"
+              data-source="${item?.original}"
+              alt="${item?.description}"
+            />
+        </a>
+      </div>`
+  )
+  .join('');
+
+galleryRef.innerHTML = renderedGallery;
+
+galleryRef.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const modal = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="800" height="600">`
+  );
+
+  modal.show();
+  window.addEventListener('keydown', closeModalOnEsc);
+
+  function closeModalOnEsc(e) {
+    if (e.keyCode === 27) {
+      modal.close();
+      window.removeEventListener('keydown', closeModalOnEsc);
+    }
+  }
+});
